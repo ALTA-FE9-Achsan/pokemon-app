6139a409d9aab0efb88afce8ecda84cb701c28f4
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-10-14 08:46:07 +0700

    revisi

--- src/pages/MyPokemon.jsx
+++ src/pages/MyPokemon.jsx
@@ -6,7 +6,7 @@ import { WithRouter } from "utils/Navigation";
 function MyPokemon(props) {
   return (
     <Container>
-      <h1>Halo</h1>;
+      <h1>Halaman belum tersedia</h1>;
     </Container>
   );
 }
